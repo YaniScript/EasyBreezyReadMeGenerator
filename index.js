d76472fc4127ec28de5870b3e1968746529a7621
Dayana Montgomery <dayanamontgomery@Dayanas-MacBook-Air.local>
2023-08-02 15:58:48 -0400

    it's workingggg! The prompts are working, the answers are embedded into a html file, now time for the badge and some tidying up

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs/promises");
 
-function dynamicHTML () {
+function dynamicHTML (answers) {
     return `<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -45,23 +45,23 @@ function dynamicHTML () {
     </style>
     
     <body style="background-color: #5a5a5a; font-family: 'Courier New', Courier, monospace">
-        <h1>Project Title</h1>
-        <h3>Project Description</h3>
+        <h1>${answers.Title}</h1>
+        <h3>${answers.Description}</h3>
         <br>
         <h2>Table of Contents</h2>
         <h4>Installation Instructions</h4>
-        <p>Some information about how to install the application, and the video.</p>
+        <p>${answers.Installation}</p>
         <h4>Usage Information</h4>
-        <p>Some Information about how to use the application/program</p>
+        <p>${answers.Usage}</p>
         <h4>Contribution Guidelines</h4>
-        <p>Some information about how to contribute to the application</p>
+        <p>${answers.Contribution}</p>
         <br>
         <h2>Contact Information</h2>
-        <p>A message from the creator about contact information</p>
+        <p>${answers.Contact}</p>
         <h4>GitHub Username</h4>
-        <p>Creator's github username</p>
+        <p>${answers.Github}</p>
         <h4>Email Address</h4>
-        <p>Creator's email address</p>
+        <p>${answers.Email}</p>
     
     
     
@@ -69,59 +69,63 @@ function dynamicHTML () {
     </html>`
 }
 
-fs.writeFile("ReadMe.html", )
-
 // TODO: Create an array of questions for user input
-// const questions = [
-//     inquirer
-//         .prompt ([ 
-//         {
-//             type: 'input',
-//             message: 'What is the title of the project?',
-//             name: 'Project Title',
-//         },
-//         {   type: 'input',
-//             message: 'Give a short description of the project.',
-//             name: 'Project Description',
-//         },
-//         {
-//             type: 'input',
-//             message: 'How do you install this project? Provide installation instructions.',
-//             name: 'Installation Instructions',
-//         },
-//         {
-//             type: 'input',
-//             message: 'How should a user interact with this project/application?',
-//             name: 'Usage Information',
-//         },
-//         {
-//             type: 'input',
-//             message: 'If users are able to contribute to this application, what should they know about ethically contributing to this project?',
-//             name: 'Contribution Guidelines',
-//         },
-//         {
-//             type:'input',
-//             message: 'What would you like to tell users about contacting you?',
-//             name: 'Contact Information Message'
-//         },
-//         {
-//             type: 'input',
-//             message: 'What is your GitHub username?',
-//             name: 'Github Information'
-//         },
-//         {
-//             type: 'input',
-//             message: 'What is your email address?',
-//             name: 'Email Address'
-//         }
-//         ])
-//         .then((answers) => {
-//             console.log(answers);
-//         })
-//         .catch((error) => {
-//             console.log(error);
-//         })
-//     ]
+const questions = [
+    inquirer
+        .prompt ([ 
+        {
+            type: 'input',
+            message: 'What is the title of the project?',
+            name: 'Title',
+        },
+        {   type: 'input',
+            message: 'Give a short description of the project.',
+            name: 'Description',
+        },
+        {
+            type: 'input',
+            message: 'How do you install this project? Provide installation instructions.',
+            name: 'Installation',
+        },
+        {
+            type: 'input',
+            message: 'How should a user interact with this project/application?',
+            name: 'Usage',
+        },
+        {
+            type: 'input',
+            message: 'If users are able to contribute to this application, what should they know about ethically contributing to this project?',
+            name: 'Contribution',
+        },
+        {
+            type:'input',
+            message: 'What would you like to tell users about contacting you?',
+            name: 'Contact'
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'Github'
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'Email'
+        },
+        ])
+        .then((answers) => {
+            // console.log(answers);
+            const makeHTML = dynamicHTML(answers);
+    
+        fs.writeFile("ReadMe.html", makeHTML) 
+            .then(() => console.log("File Created!"))
+            .catch((err) => console.error(err))
+
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    ]
 
     
 
@@ -132,4 +136,4 @@ fs.writeFile("ReadMe.html", )
 // function init() {}
 
 // // Function call to initialize app
-// init();
+// init()
